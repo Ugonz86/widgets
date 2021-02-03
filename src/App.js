6701d6e067ea6181822bb9aef94d29befde054b9
Ugonz86 <ugonzalez86@gmail.com>
2021-02-02 18:10:13 -0800

    Initia; commit, project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Accordion from './components/Accordion';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript framework'
+  },
+  {
+    title: 'Why is React?',
+    content: 'React is a favorite JS library among engineers'
+  },
+  {
+    title: 'Ho do you use React?',
+    content: 'You use React by creating components'
+  }
+];
 
-export default App;
+export default () => {
+  return (
+    <div>
+      <br />
+      <Accordion items={items} />
+    </div>
+  )
+};
+
