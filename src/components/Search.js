8df1c0cb5b0fd979c89987823ed45e757a7dc3e4
Ugonz86 <ugonzalez86@gmail.com>
2021-02-08 14:06:22 -0800

    Search bar set without default value, add a timeout, add a useEffect cleanup function and implement a delayed request

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('programming');
+    const [term, setTerm] = useState('');
     const [results, setResults] = useState([]);
 
     // console.log(results);
@@ -22,21 +22,33 @@ const Search = () => {
             setResults(data.query.search);
         };
 
-        //if term is not set to a default value, use this below
-        // if (term) {
-        //     search();
-        // }
-        search();
+        const timeoutId = setTimeout(() => {
+            if (term) {
+                search();
+            }
+
+        }, 500);
+
+        return () => {
+            clearTimeout(timeoutId);
+        };
+
     }, [term]);
 
     const renderedResults = results.map((result) => {
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                </a>
+                </div>
                 <div className="content">
-                    <div className="header">
-                        {result.title}
-                    </div>
-                    {result.snippet}
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
                 </div>
             </div>
         );
