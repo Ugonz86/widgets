a293fdfbaf5b781e4f7eb38265cd316cbe60444c
Ugonz86 <ugonzalez86@gmail.com>
2021-02-08 14:37:46 -0800

    Refactor Search functionality using useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
-    // console.log(results);
+    //If user types something, 
+    //immediately update 'term' 
+    //and set a timer to update 'debouncedTerm'.
+    //If user types something, 
+    //cancel previous timer, 
+    //immediately update term 
+    //and set a timer to update 'debouncedTerm'.
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -15,25 +31,14 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
 
             setResults(data.query.search);
         };
-
-        const timeoutId = setTimeout(() => {
-            if (term) {
-                search();
-            }
-
-        }, 500);
-
-        return () => {
-            clearTimeout(timeoutId);
-        };
-
-    }, [term]);
+        search();
+    }, [debouncedTerm]);
 
     const renderedResults = results.map((result) => {
         return (
