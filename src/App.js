11fd3437397b9410b6f94e4c236d778004161ffd
Ugonz86 <ugonzalez86@gmail.com>
2021-02-08 12:06:52 -0800

    Added async code in useEffect and default term term

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
-import Accordion from './components/Accordion';
-// import Search from './components/Search';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 // import './App.css';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
-  },
-  {
-    title: 'Why is React?',
-    content: 'React is a favorite JS library among engineers'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
-  }
-];
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework'
+//   },
+//   {
+//     title: 'Why is React?',
+//     content: 'React is a favorite JS library among engineers'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components'
+//   }
+// ];
 
-export default () => {
+const App = () => {
   return (
     <div>
       <br />
-      <Accordion items={items} />
-      {/* <Search /> */}
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   )
 };
+
+export default App;
 
