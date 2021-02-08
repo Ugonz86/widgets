813327455146624eb5699277bdf716790d11d329
Ugonz86 <ugonzalez86@gmail.com>
2021-02-08 14:47:49 -0800

    Create Dropdown widget/component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 // import './App.css';
 
 // const items = [
@@ -18,12 +19,29 @@ import Search from './components/Search';
 //   }
 // ];
 
+const options = [
+  {
+      label: 'The color Red',
+      value: 'red'
+  },
+  {
+      label: 'The color Green',
+      value: 'green'
+  },
+  {
+      label: 'A shade or Blue',
+      value: ''
+  },
+
+];
+
 const App = () => {
   return (
     <div>
       <br />
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options}/>
     </div>
   )
 };
---
+++ src/components/Dropdown.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Dropdown = () => {
+
+    return <h1>Dropdown</h1>
+
+};
+
+export default Dropdown;
