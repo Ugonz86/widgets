14baf3876ba81d75c17968439d4789e4ff8005d7
Ugonz86 <ugonzalez86@gmail.com>
2021-02-08 17:47:22 -0800

    Implement open/hide handler to the option list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        });
+    }, []);
+
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
             return null;
         }
-        
+
         return (
-            <div 
-            key={option.value} 
-            className="item"
-            onClick={() => onSelectedChange(option)}
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option)}
             >
                 {option.label}
             </div>
@@ -18,13 +30,16 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     });
 
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a color</label>
-                <div className="ui selection dropdown visible active">
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>
                         {renderedOptions}
                     </div>
                 </div>
